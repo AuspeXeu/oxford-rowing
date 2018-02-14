bebabd7c0b726244503d6c2e32863f2de9e0432e
Chris Vaas <chrisvaas@gmail.com>
2018-02-14 18:48:30 +0000

    Dispose websocket on error

--- server.js
+++ server.js
@@ -89,7 +89,10 @@ app.ws('/live', (ws, req) => {
       clients.forEach((ws) => ws.send(JSON.stringify({type: 'reporters', number: reporters.size})))
     }
   })
-  ws.on('error', (err) => log(err))
+  ws.on('error', (err) => {
+    log(err)
+    clients.delete(id)
+  })
   ws.on('message', (msg) => {
     msg = JSON.parse(msg)
     if (msg.type === 'reporter' && isAuth(msg.auth)) {
