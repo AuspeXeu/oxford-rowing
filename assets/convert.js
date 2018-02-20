89e652fb1312a7300b71c20c0871731bc8a848c7
Chris Vaas <chrisvaas@gmail.com>
2018-02-20 10:24:22 +0000

    Added csv to json conversion script

---
+++ assets/convert.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+
+const inMen = 'torpids_2017_men.csv'
+const inWomen = 'torpids_2017_women.csv'
+const out = '../data/torpids_2017.json'
+
+const data = {}
+const men = fs.readFileSync(`./${inMen}`, 'utf8')
+const women = fs.readFileSync(`./${inWomen}.csv`, 'utf8')
+
+const proc = (csv, gender) => {
+  csv.split('\n').map((line) => line.replace('\r','').split(',')).forEach((itm, idx) => {
+    if (!data[itm[0]])
+      data[itm[0]] = {men: [], women:[]}
+    data[itm[0]][gender].push({
+      start: idx + 1,
+      moves: itm.slice(1, itm.length).map((move) => parseInt(move, 10))
+    })
+  })
+}
+
+proc(men, 'men')
+proc(women, 'women')
+fs.writeFileSync(file, JSON.stringify(data), 'utf8')
