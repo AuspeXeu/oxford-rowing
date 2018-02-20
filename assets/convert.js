f1ef7f5bc2489660b41e696943ecd1a1fc4ef309
Chris Vaas <chrisvaas@gmail.com>
2018-02-20 10:36:53 +0000

    Fixed data

--- assets/convert.js
+++ assets/convert.js
@@ -10,9 +10,10 @@ const women = fs.readFileSync(`./${inWomen}`, 'utf8')
 
 const proc = (csv, gender) => {
   csv.split('\n').map((line) => line.replace('\r','').split(',')).forEach((itm, idx) => {
-    if (!data[itm[0]])
-      data[itm[0]] = {men: [], women:[]}
-    data[itm[0]][gender].push({
+    const club = itm[0].trim()
+    if (!data[club])
+      data[club] = {men: [], women:[]}
+    data[club][gender].push({
       start: idx + 1,
       moves: itm.slice(1, itm.length).map((move) => parseInt(move, 10))
     })
