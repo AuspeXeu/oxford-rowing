156bdef0e816a9c423b821c4ffd7d24660432383
Chris Vaas <chrisvaas@gmail.com>
2018-02-23 15:54:07 +0000

    Confirm old days

--- server.js
+++ server.js
@@ -71,6 +71,12 @@ const updateEntry = (data, name, year, club, gender, number, day, move) => {
       entry.status = move.status
     }
   data[club][gender][number].moves[day-1] = entry
+  data[club][gender][number].moves.slice(0, day-1).forEach((move, idx) => {
+    move.status = true
+    const payload = {type: 'update', name: name,year: year,club: club,gender: gender,number: number,day: idx+1,move: move}
+    broadcast(payload)
+    data[club][gender][number].moves[idx] = move
+  })
   const payload = {type: 'update', name: name,year: year,club: club,gender: gender,number: number,day: day,move: data[club][gender][number].moves[day-1]}
   broadcast(payload)
   return true
