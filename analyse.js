fbe3aa6e7ab238d2f8cf7486951918bd712acc9e
Chris Vaas <chrisvaas@gmail.com>
2018-03-04 21:11:05 +0000

    Added analysis

---
+++ analyse.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+const request =  require('request')
+
+const clients = fs.readFileSync('./clients.csv', 'utf8')
+const events = clients.split('\n').map((itm) => ({ip: itm[1], timestamp: itm[2], type: itm[0]}))
+
+const ips = events.filter((evt) => evt.type === 'c')
+
+const uqIps = new Set(ips)
+
+const geoLookUp = (ip) => {
+  return new Promise ((resolve, reject) => {
+    request('https://geo.auspex.eu', (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      body = JSON.parse(body)
+      console.log(body)
+      resolve({ip: body.ip, country: body.country.names.en, country_code: body.country.iso_code})
+    } else {
+      console.log(error, response.statusCode)
+      reject(error)
+    }
+    })
+  })
+}
+
+const promises = Array.from(uqIps).map((ip) => geoLookUp(ip))
+
+Promise.all(promises).then((data) => {
+  console.log(data)
+}).catch((err) => {
+  console.log(err)
+})
+
