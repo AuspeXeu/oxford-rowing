6e4ce662f73f2e3afad2c62f195be99041ede165
Chris Vaas <christianvaas@auspex.eu>
2019-11-24 22:22:04 -0500

    feat(App): announce new historic data

--- src/App.vue
+++ src/App.vue
@@ -375,7 +375,7 @@ export default {
         {key: 'localtime', text: 'Race times are displayed in your local time :)'},
         {key: 'crewlist', text: `Click on a boat's crest to see its crew list!`},
         {key: 'crewlisthist', text: 'We now added crew lists for all events back until 2017'},
-        {key: 'spoons', text: `Thanks to Harry Mellor (Hugh's) we now also have a spoons filter!`}
+        {key: 'history', text: 'We added much more historic data as far as 1826!'}
       ],
       feature: null,
       reporters: 0,
